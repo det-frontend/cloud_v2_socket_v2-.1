da671f88a5280fc6e70621fd979dceae190cb933
HtooKhant-vexix <vexixongaming02@gmail.com>
2024-06-27 19:45:06 +0000

    G

--- config/default.ts
+++ config/default.ts
@@ -4,9 +4,9 @@ export default {
   controlDbUrl:
     "mongodb+srv://ksdbfms:Tpd7iFyquVJrh4u8@controller.zabfxgj.mongodb.net/?retryWrites=true&w=majority",
   kyawsan_DbUrl:
-    "mongodb://detpos:asdffdsa@127.0.0.1:27017/kyawsan?authSource=admin",
+    "mongodb+srv://ksdbfms:Asdffdsa-4580@ksfms.tfk72hu.mongodb.net/?retryWrites=true&w=majority",
   common_DbUrl:
-    "mongodb://detpos:asdffdsa@127.0.0.1:27017/common?authSource=admin",
+    "mongodb+srv://ksdbfms:C9GADG2k0tLRl95C@common.advclns.mongodb.net/?retryWrites=true&w=majority",
   saltWorkFactor: 10,
   secretKey: "suuhh",
   page_limit: 50,
