3a6f24a7f23d4129861debe05e02ff3d854bb2fc
Htet Aung Khant <htetaungkhant@securelinkmm.com>
2024-07-30 16:13:47 +0630

    pm2 config update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,9 +3,8 @@ module.exports = {
       {
         name: 'se-sai',
         script: 'node dist/app.js', // Change this to the entry point of your application
-        node_args: '--max-old-space-size=4098', // Increase memory limit
         watch: false, // Watch files for changes (optional)
-        max_memory_restart: '4G', // Restart if it exceeds this memory
+        max_memory_restart: '1G', // Restart if it exceeds this memory
         env: {
           NODE_ENV: 'development',
         },
