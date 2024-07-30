301141e82423e80d4eb5126f0169a6702fcb4d4a
Htet Aung Khant <htetaungkhant@securelinkmm.com>
2024-07-30 16:18:55 +0630

    pm2 config update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,10 @@ module.exports = {
         name: 'se-sai',
         script: 'node dist/app.js', // Change this to the entry point of your application
         watch: false, // Watch files for changes (optional)
+        instance: 1,
         max_memory_restart: '1G', // Restart if it exceeds this memory
+        autorestart: true,
+        cron_restart: '0 0 * * *',
         env: {
           NODE_ENV: 'development',
         },
