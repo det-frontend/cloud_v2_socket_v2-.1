c18f2ecda89aa9adc216e3c9bfca48bb054dc6c8
Htet Aung Khant <htetaungkhant@securelinkmm.com>
2025-01-16 16:41:12 +0630

    default.ts

--- config/default.ts
+++ config/default.ts
@@ -2,15 +2,15 @@ export default {
   port: 9000,
   host: "localhost",
   controlDbUrl:
-    "mongodb+srv://ksdbfms:Ok5mh7QG2w8s@controller.zabfxgj.mongodb.net/?retryWrites=true&w=majority",
+    "mongodb+srv://ksdbfms:Tpd7iFyquVJrh4u8@controller.zabfxgj.mongodb.net/?retryWrites=true&w=majority",
   // kyawsan_DbUrl:
   //   "mongodb://detpos:asdffdsa@127.0.0.1:27017/HKTest?authSource=admin",
   kyawsan_DbUrl:
-    "mongodb+srv://ksdbfms:U2d7D1to0VFu@ksfms.tfk72hu.mongodb.net/?retryWrites=true&w=majority",
+    "mongodb+srv://ksdbfms:Asdffdsa-4580@ksfms.tfk72hu.mongodb.net/?retryWrites=true&w=majority",
   // common_DbUrl:
   //   "mongodb://detpos:asdffdsa@127.0.0.1:27017/common?authSource=admin",
   common_DbUrl:
-    "mongodb+srv://ksdbfms:Fi9TZTZq10C1@common.advclns.mongodb.net/?retryWrites=true&w=majority",
+    "mongodb+srv://ksdbfms:C9GADG2k0tLRl95C@common.advclns.mongodb.net/?retryWrites=true&w=majority",
   saltWorkFactor: 10, 
   secretKey: "suuhh",
   page_limit: 50,
